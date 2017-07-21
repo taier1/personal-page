80d41245f5c5dc6427eaf8167a28aa2cc22434ff
Alexander Fischer <alexander.fischer@usi.ch>
2017-07-21 12:13:02 +0200

    Added project images.

--- js/scroll.js
+++ js/scroll.js
@@ -1,12 +1,12 @@
 function smoothScroll(e) {
-    let internal = e.currentTarget.classList.contains(".scroll");
-    if (internal) { // check if the link is internal
-        e.preventDefault();
-        // from: https://css-tricks.com/snippets/jquery/smooth-scrolling/
-        document.querySelector(e.currentTarget.hash).scrollIntoView({
-            behavior: 'smooth'
-        });
-    }
+  let internal = e.currentTarget.classList.contains(".scroll");
+  if (internal) { // check if the link is internal
+    e.preventDefault();
+    // from: https://css-tricks.com/snippets/jquery/smooth-scrolling/
+    document.querySelector(e.currentTarget.hash).scrollIntoView({
+      behavior: 'smooth'
+    });
+  }
 }
 
 $("body").on("click", "a", smoothScroll);
