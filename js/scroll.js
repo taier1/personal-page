2dbbe07942c8abdfa4af4ceea88fd66ea831f524
Alexander Fischer <alexander.fischer@usi.ch>
2017-09-08 13:04:58 +0200

    Fixed smooth scroll for Chrome (now using jQuery).

--- js/scroll.js
+++ js/scroll.js
@@ -1,11 +1,9 @@
 function smoothScroll(e) {
   let internal = e.currentTarget.classList.contains(".scroll");
   if (internal) { // check if the link is internal
-    e.preventDefault();
-    // from: https://css-tricks.com/snippets/jquery/smooth-scrolling/
-    document.querySelector(e.currentTarget.hash).scrollIntoView({
-      behavior: 'smooth'
-    });
+    $('html, body').animate({
+      scrollTop: $(e.currentTarget.hash).offset().top // makes it g
+    }, 400, 'swing');
   }
 }
 
